224393a48b962da492b5eabce82fa1c1abae0073
lipengzhou <lpzmail@163.com>
2020-01-09 16:38:28 +0800

    chore: add preview account

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -79,8 +79,8 @@ export default {
   data () {
     return {
       user: {
-        mobile: '',
-        code: ''
+        mobile: '13911111111',
+        code: '246810'
       },
       isSending: false
     }
