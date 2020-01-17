8ed217187ae5cad6f46a5bf776e93c1d23d7fbe0
lipengzhou <lpzmail@163.com>
2020-01-17 18:30:46 +0800

    refactor: user-profile

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -4,14 +4,12 @@
       class="page-navbar"
       title="个人信息"
       left-arrow
-      @click-left="$router.back()"
     />
     <van-cell-group>
       <van-cell title="头像" is-link @click="onShowFile">
         <van-image
+          class="avatar"
           round
-          width="30"
-          height="30"
           :src="user.photo"
         />
       </van-cell>
@@ -20,77 +18,46 @@
         title="昵称"
         :value="user.name"
         is-link
-        @click="isNameShow = true"
-      />
-      <van-cell
-        title="介绍"
-        :value="user.intro"
-        is-link
-        @click="isIntroShow = true"
+        @click="isEditNameShow = true"
       />
       <van-cell
         title="性别"
         :value="user.gender === 0 ? '男' : '女'"
         is-link
-        @click="isGenerShow = true"
+        @click="isEditGenerShow = true"
       />
       <van-cell
         title="生日"
         :value="user.birthday"
         is-link
-        @click="isBirthdayShow = true"
+        @click="isEditBirthdayShow = true"
       />
     </van-cell-group>
-
-    <!-- 修改昵称 -->
-    <van-dialog
-      v-model="isNameShow"
-      title="昵称修改"
-      show-cancel-button
-      @confirm="onNameConfirm"
-    >
-      <!--
-        v-model 是
-          :value="user.name"
-          @input="user.name = 事件参数"
-       -->
-      <van-field
-        :value="user.name"
-        placeholder="请输入用户名"
-        @input="inputName = $event"
-      />
-    </van-dialog>
-    <!-- /修改昵称 -->
 
     <!-- 修改介绍 -->
     <van-popup
       class="intro-popup"
-      v-model="isIntroShow"
+      v-model="isEditNameShow"
       position="bottom"
       get-container="body"
-      :style="{ height: '30%' }"
     >
       <van-nav-bar
-        title="介绍"
+        title="昵称"
         left-text="取消"
         right-text="完成"
-        @click-left="isIntroShow = false"
-        @click-right="() => {
-          this.user.intro = inputIntro
-          this.onSave()
-          this.isIntroShow = false
-        }"
+        @click-left="isEditNameShow = false"
+        @click-right="onNameConfirm"
       >
       </van-nav-bar>
       <div class="intro-field-wrap">
         <van-field
-          :value="user.intro"
-          @input="inputIntro = $event"
+          :value="user.name"
+          @input="inputName = $event"
           rows="2"
           autosize
           type="textarea"
-          maxlength="50"
-          placeholder="请输入介绍"
+          maxlength="20"
+          placeholder="请输入昵称"
           show-word-limit
         />
       </div>
@@ -99,7 +66,7 @@
 
     <!-- 修改性别 -->
     <van-action-sheet
-      v-model="isGenerShow"
+      v-model="isEditGenerShow"
       :actions="actions"
       @select="onGenerSelect"
       cancel-text="取消"
@@ -108,49 +75,59 @@
 
     <!-- 修改生日 -->
     <van-popup
-      v-model="isBirthdayShow"
+      v-model="isEditBirthdayShow"
       position="bottom"
-      :style="{ height: '35%' }"
     >
       <van-datetime-picker
         v-model="currentDate"
         type="date"
         :min-date="minDate"
         @confirm="onBirthdayConfirm"
-        @cancel="isBirthdayShow = false"
+        @cancel="isEditBirthdayShow = false"
       />
     </van-popup>
     <!-- /修改生日 -->
+
+    <van-image-preview
+      v-model="isPreviewPhotoShow"
+      :images="previewImages"
+    >
+      <van-nav-bar
+        slot="cover"
+        left-text="取消"
+        right-text="确定"
+      />
+    </van-image-preview>
   </div>
 </template>
 
 <script>
 import {
   getProfile,
-  updateUserPhoto,
   updateUserProfile,
   getSelf
 } from '@/api/user'
 import dayjs from 'dayjs'
 
 export default {
-  name: 'UserIndex',
+  name: 'UserProfile',
   data () {
     return {
       user: {
       },
+      isEditNameShow: false,
       isNameShow: false,
       inputName: '',
-      isGenerShow: false,
+      isEditGenerShow: false,
       actions: [
         { name: '男', value: 0 },
         { name: '女', value: 1 }
       ],
-      isBirthdayShow: false,
+      isEditBirthdayShow: false,
       currentDate: new Date(),
       minDate: new Date(1970, 1, 1),
-      isIntroShow: false,
-      inputIntro: ''
+      isPreviewPhotoShow: false,
+      previewImages: []
     }
   },
   computed: {
@@ -173,52 +150,70 @@ export default {
     },
 
     onFileChange () {
-      this.user.photo = URL.createObjectURL(this.file.files[0])
+      const fileObj = URL.createObjectURL(this.file.files[0])
+      // 清空数组
+      this.previewImages = []
+      // 添加预览图片
+      this.previewImages.push(fileObj)
+      // 显示预览
+      this.isPreviewPhotoShow = true
     },
 
-    async onSave () {
+    async saveProfile (field, value) {
       this.$toast.loading({
         duration: 0, // 持续时间，0表示持续展示不停止
         forbidClick: true, // 是否禁止背景点击
         message: '保存中...' // 提示消息
       })
+
       try {
-        // 保存头像
-        const fileObj = this.file.files[0]
-        // 如果用户选了新的头像才更新头像
-        if (fileObj) {
-          const fd = new FormData()
-          fd.append('photo', fileObj)
-          await updateUserPhoto(fd)
-        }
-        const { name, gender, birthday, intro } = this.user
         await updateUserProfile({
-          name,
-          gender,
-          birthday,
-          intro
+          [field]: value
         })
         this.$toast.success('保存成功')
       } catch (err) {
-        this.$toast.success('保存失败', err)
+        this.$toast.success('保存失败')
+        return Promise.reject(err)
       }
     },
 
-    onNameConfirm () {
-      this.user.name = this.inputName
+    async onNameConfirm () {
+      try {
+        // 请求更新
+        await this.saveProfile('name', this.inputName)
+
+        // 更新视图
+        this.user.name = this.inputName
+
+        // 关闭弹层
+        this.isEditNameShow = false
+      } catch (err) {
+        console.log(err)
+      }
     },
 
-    onGenerSelect (item) {
-      // 更新数据
+    async onGenerSelect (item) {
+      // 请求更新
+      await this.saveProfile('gender', item.value)
+
+      // 更新视图
       this.user.gender = item.value
 
       // 关闭弹层
-      this.isGenerShow = false
+      this.isEditGenerShow = false
     },
 
-    onBirthdayConfirm (value) {
-      this.user.birthday = dayjs(value).format('YYYY-MM-DD')
-      this.isBirthdayShow = false
+    async onBirthdayConfirm (value) {
+      const birthday = dayjs(value).format('YYYY-MM-DD')
+
+      // 请求更新
+      await this.saveProfile('birthday', birthday)
+
+      // 更新视图
+      this.user.birthday = birthday
+
+      // 关闭弹层
+      this.isEditBirthdayShow = false
     }
   }
 }
@@ -229,6 +224,21 @@ export default {
   background-color: #f5f7f9;
   .intro-field-wrap {
     padding: 10px;
+  }
+}
+
+.avatar {
+  width: 30px;
+  height: 30px;
+}
+
+/deep/ .van-image-preview__cover {
+  left: 0;
+  bottom: 0;
+  right: 0;
+  top: unset;
+  .van-nav-bar {
+    background: #000;
   }
 }
 </style>
